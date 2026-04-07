ac69190426bcb3a6b46095e27799b447c664038d
Phạm Nguyễn Phú Thành <thanhphamnguyenphu2k5@gmail.com>
2026-04-07 19:13:58 +0700

    Hotfix(Layout): not render

---
+++ Bai01verClaude/app/src/main/java/com/goldtracker/ui/chart/ChartFragment.java
@@ -0,0 +1,22 @@
+package com.goldtracker.ui.chart;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ChartFragment extends Fragment {
+    @Nullable @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        TextView tv = new TextView(getContext());
+        tv.setText("Chart");
+        tv.setTextColor(0xFFFFFFFF);
+        return tv;
+    }
+}
